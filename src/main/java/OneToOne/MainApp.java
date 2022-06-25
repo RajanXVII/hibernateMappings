1672a9d679244b70b00fc1fe097f82e506048516
'RajanXVII' <a839348@DESKTOP-DN1RNVP>
2022-06-25 21:06:00 +0530

    Adding save functionality

---
+++ src/main/java/OneToOne/MainApp.java
@@ -0,0 +1,36 @@
+package OneToOne;
+
+import java.sql.SQLException;
+import java.util.Objects;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import OneToOne.Entities.Instructor;
+import OneToOne.Entities.InstructorDetail;
+
+public class MainApp {
+	public static void main(String[] args) throws SQLException {
+		SessionFactory sf= new 	Configuration().configure().addAnnotatedClass(InstructorDetail.class)
+				.addAnnotatedClass(Instructor.class).buildSessionFactory();
+		
+		Session sess=sf.getCurrentSession();
+		sess.beginTransaction();
+		
+		  Instructor instructor=new Instructor("pratik", "sonvane",
+		  "pratik17.rs@gmail.com"); InstructorDetail instructorDetail=new
+		  InstructorDetail("pratikyt", "teaching");
+		 
+		int id=4;
+		//Instructor instructor=sess.get(Instructor.class, id);
+		instructor.setInstructorDetail(instructorDetail);
+		sess.save(instructor);
+		//sess.delete(instructor);
+		/*
+		 * if(Objects.nonNull(instructor)) { System.out.println("deleted!!"); }
+		 */
+		System.out.println("persisted!!");
+		sess.getTransaction().commit();
+	}
+}
